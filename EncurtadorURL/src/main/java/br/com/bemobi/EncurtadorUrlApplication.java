fdd66e8758311ee8f9b2fbacf0aa136a204366c9
arodrigues32 <arodrigues32@BRBGRNF22.CSCMWS.CSCMWS.COM>
2019-05-10 17:14:43 -0300

    Commit inicial

---
+++ EncurtadorURL/src/main/java/br/com/bemobi/EncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package br.com.bemobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+
+}
